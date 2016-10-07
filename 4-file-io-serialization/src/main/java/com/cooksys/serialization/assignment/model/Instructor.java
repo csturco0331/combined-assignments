cd51a5cea93dcf3efa322239af72b2e4ee8c56e7
Cory Turco <csturco0331@yahoo.com>
2016-10-07 15:18:14 -0500

    Assignment 4 finished

--- 4-file-io-serialization/src/main/java/com/cooksys/serialization/assignment/model/Instructor.java
+++ 4-file-io-serialization/src/main/java/com/cooksys/serialization/assignment/model/Instructor.java
@@ -1,13 +1,28 @@
 package com.cooksys.serialization.assignment.model;
 
 public class Instructor {
-    private Contact contact;
+	private Contact contact;
 
-    public Contact getContact() {
-        return contact;
-    }
+	public Instructor() {
 
-    public void setContact(Contact contact) {
-        this.contact = contact;
-    }
+	};
+
+	public Instructor(Contact contact) {
+		this.contact = contact;
+	}
+
+	public Instructor(String firstName, String lastName, String email, String phoneNumber) {
+		contact.setFirstName(firstName);
+		contact.setLastName(lastName);
+		contact.setEmail(email);
+		contact.setPhoneNumber(phoneNumber);
+	}
+
+	public Contact getContact() {
+		return contact;
+	}
+
+	public void setContact(Contact contact) {
+		this.contact = contact;
+	}
 }
--- 4-file-io-serialization/src/main/java/com/cooksys/serialization/assignment/model/Student.java
+++ 4-file-io-serialization/src/main/java/com/cooksys/serialization/assignment/model/Student.java
@@ -1,7 +1,23 @@
 package com.cooksys.serialization.assignment.model;
 
+
 public class Student {
     private Contact contact;
+    
+    public Student() {
+    	
+    };
+    
+    public Student(Contact contact) {
+    	this.contact = contact;
+    }
+    
+    public Student(String firstName, String lastName, String email, String phoneNumber) {
+    	contact.setFirstName(firstName);
+    	contact.setLastName(lastName);
+    	contact.setEmail(email);
+    	contact.setPhoneNumber(phoneNumber);
+    }
 
     public Contact getContact() {
         return contact;
